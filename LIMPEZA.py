74b3bbe658b3974c560f4265eba2af67c9367f90
leticiagraziellegomes <102824656+leticiagraziellegomes@users.noreply.github.com>
2022-05-07 18:30:22 -0300

    Add files via upload

---
+++ LIMPEZA.py
@@ -0,0 +1,14 @@
+largura = input("coloque a largura: ")
+
+comprimento = input("coloque a comprimento: ")
+
+total = float(largura) * float(comprimento)
+
+produtoLitros = total / 2
+
+print("Sao necessarios " + str(produtoLitros) + "litros de produtos")
+
+
+
+
+
